398475df5962fbb09c82df310a4f6d6c1ce34a1c
Samana0104 <b_hb0104@naver.com>
2025-03-06 20:51:54 +0900

    remove a test client

--- TestClient/TNetClient.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "TNetwork.h"
-class TNetClient :   public TNetwork
-{
-public:
-    void   Run() override;
-};
-
--- TestClient/TNetServer.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "TNetwork.h"
-class TNetServer : public TNetwork
-{
-};
-
